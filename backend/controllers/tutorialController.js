99311b7e196fc342f384a9c52be91cc62219fb1d
Mateusz Redestowicz <mati.red@gmail.com>
2022-11-06 13:30:58 +0100

    add authorizated changes tutorials

--- backend/controllers/tutorialController.js
+++ backend/controllers/tutorialController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 
 const Tutorial = require('../models/tutorialModel')
+const User = require('../models/userModel')
 
 
 // @desc Get tutorials
@@ -14,23 +15,41 @@ const getTutorials = asyncHandler (async (req, res) => {
 // @desc Set tutorials
 // @route SET /api/tutorials
 const setTutorials = asyncHandler (async (req, res) => {
+
     if(!req.body.text){
         res.status(400)
         throw new Error('please add a text field')
     }
-    const tutorial = await Tutorial.create({text: req.body.text})
+    const tutorial = await Tutorial.create({
+        text: req.body.text,
+        user: req.user.id,
+    })
     res.status(200).json(tutorial)
 })
 
 // @desc update tutorial
 // @route PUT /api/tutorials/:id
 const updateTutorial = asyncHandler (async (req, res) => {
+
 const tutorial = await Tutorial.findById(req.params.id)
 
     if(!tutorial){
         res.status(400)
         throw new Error('not found')
     }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('user not found')
+    }
+
+    if(tutorial.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
     const updatedTutorial = await Tutorial.findByIdAndUpdate(req.params.id, req.body,{
         new: true,
     })
@@ -49,6 +68,20 @@ const deleteTutorial = asyncHandler (async (req, res) => {
         res.status(400)
         throw new Error('tutorial doesn`t exist')
     }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('user not found')
+    }
+
+    if(tutorial.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    
     tutorial.remove()
     res.status(200).json(tutorial.text)
 })
--- backend/routes/tutorialRoutes.js
+++ backend/routes/tutorialRoutes.js
@@ -2,13 +2,15 @@ const express = require('express')
 const {getTutorials, setTutorials, updateTutorial, deleteTutorial} = require('../controllers/tutorialController')
 const router = express.Router()
 
+const {protect} = require('../middleware/authModdleware')
+
 router.get('/', getTutorials)
 
-router.post('/', setTutorials)
+router.post('/', protect, setTutorials)
 
-router.put('/:id', updateTutorial)
+router.put('/:id', protect, updateTutorial)
 
 
-router.delete('/:id', deleteTutorial)
+router.delete('/:id',protect, deleteTutorial)
 
 module.exports = router
