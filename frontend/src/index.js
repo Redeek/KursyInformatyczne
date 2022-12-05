7dcd208bed3176d6bb168097619c3e6f29f09894
Mateusz Redestowicz <mati.red@gmail.com>
2022-12-05 22:14:42 +0100

    Add chapters

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,6 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
